abf4fd7694680f61b8da78a3ea7e8ddda9fda9c2
randy201 <108166894+randy201@users.noreply.github.com>
2023-11-23 15:40:32 +0300

    validation

--- Facture.sql
+++ Facture.sql
@@ -6,7 +6,25 @@ date Validation
 
 
 table etat:
-    10 en attente
+    0 refuser
     20 valide par chef de departement
     30 valide par chef departement achat
     40 valide par Directeur Achat Financier
+
+
+-----view ---- 
+SELECT 
+    d.*,
+    COALESCE(valid.validation_etat, 0) AS validation_etat,
+    valid.validation_date,
+    COALESCE(valid.validation_id, 0) AS validation_id
+FROM 
+    demande d
+LEFT JOIN  
+    validation valid 
+    ON d.demande_id = valid.demande_id 
+WHERE 
+    COALESCE(valid.validation_etat, 0) = 0;
+
+
+SELECT  d.*,  COALESCE(valid.validation_etat, 0) AS validation_etat, valid.validation_date, COALESCE(valid.validation_id, 0) AS validation_id FROM demande d LEFT JOIN  validation valid ON d.demande_id = valid.demande_id WHERE COALESCE(valid.validation_etat, 0) = 0;

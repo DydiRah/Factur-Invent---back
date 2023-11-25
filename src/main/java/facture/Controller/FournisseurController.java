07c8e7a9907a2ef6127980913a2f4972b319348c
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-11-25 11:18:18 +0300

    huhu

--- src/main/java/facture/Controller/FournisseurController.java
+++ src/main/java/facture/Controller/FournisseurController.java
@@ -1,7 +1,9 @@
 package facture.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import facture.Modele.Fournisseur;
+import facture.Modele.Utilisateur;
+import facture.Repos.FournisseurRepository;
 import facture.Service.FournisseurService;
 import lombok.AllArgsConstructor;
 
@@ -19,6 +23,7 @@ import lombok.AllArgsConstructor;
 @CrossOrigin(origins = "http://localhost:4200/")
 public class FournisseurController {
     private final FournisseurService fournisseur_serice;
+    private final FournisseurRepository fournisseur_rep;
 
     @GetMapping
     public List<Fournisseur> getAllFournisseurs() {
@@ -29,4 +34,22 @@ public class FournisseurController {
     public Fournisseur insert(@RequestBody Fournisseur f){
         return fournisseur_serice.create(f);
     }
+
+    @PostMapping("/login")
+    public HashMap<String,Object> login(@RequestBody Fournisseur fournisseur){
+        HashMap<String,Object> resp = new HashMap<>();
+        List<Fournisseur> listFournisseur  = fournisseur_rep.findByEmail(fournisseur.getEmail());
+        if (listFournisseur.size() == 0) {
+            resp.put("message", "emailerreur");
+            return resp;
+        }
+        for (Fournisseur f : listFournisseur) {
+            if (f.getMot_passe().equals(fournisseur.getMot_passe()  )) {
+                resp.put("message", listFournisseur);
+                return resp;
+            }
+        }
+        resp.put("message", "mdperreur");
+        return resp;
+    } 
 }
--- src/main/java/facture/Repos/FournisseurRepository.java
+++ src/main/java/facture/Repos/FournisseurRepository.java
@@ -1,7 +1,12 @@
 package facture.Repos;
 
 import facture.Modele.Fournisseur;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface FournisseurRepository extends CrudRepository<Fournisseur,Integer> {
+
+    public List<Fournisseur> findByEmail(String email);
 }

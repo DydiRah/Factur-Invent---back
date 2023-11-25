c52d79f3198f357cf7e86930eaec2864b8c8e910
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-11-25 21:50:11 +0300

    nyaro

--- src/main/java/facture/Controller/ProformaController.java
+++ src/main/java/facture/Controller/ProformaController.java
@@ -6,18 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import facture.Modele.Proforma;
 import facture.Repos.ProformaRepository;
 import facture.Service.EmailService;
 
-import org.springframework.web.bind.annotation.PostMapping;
 @RestController
+@CrossOrigin("*")
 public class ProformaController {
     @Autowired
     ProformaRepository proformaRepository;
@@ -42,7 +39,6 @@ public class ProformaController {
         }
 
         return new ResponseEntity<>("Demande envoyé", HttpStatus.OK);
-        
     }
 
     @DeleteMapping("/proformas")
